c48847b01b92db8788aa30d4caa83d6b839f8a06
Seokmin Jang <smjang85@gmail.com>
2022-02-26 19:54:11 +0900

    Duck Adapter 수정
    Duck Adapter 수정

--- design_pattern_edu/src/design_pattern_edu/adaptor_facade_pattern/adapter_pattern/animal/impl/DuckAdapter.java
+++ design_pattern_edu/src/design_pattern_edu/adaptor_facade_pattern/adapter_pattern/animal/impl/DuckAdapter.java
@@ -1,14 +1,19 @@
 package design_pattern_edu.adaptor_facade_pattern.adapter_pattern.animal.impl;
 
+import java.util.Random;
+
 import design_pattern_edu.adaptor_facade_pattern.adapter_pattern.animal.Duck;
 import design_pattern_edu.adaptor_facade_pattern.adapter_pattern.animal.Turkey;
 
 public class DuckAdapter implements Turkey{
 	Duck duck;
+	Random rand;
 	
 	public DuckAdapter(Duck duck) {
 		this.duck = duck;
+		rand = new Random();
 	}
+	
 
 	@Override
 	public void gobble() {
@@ -17,6 +22,8 @@ public class DuckAdapter implements Turkey{
 
 	@Override
 	public void fly() {
-		duck.fly();
+		if(rand.nextInt(5) == 0) {
+			duck.fly();
+		}
 	}
 }
